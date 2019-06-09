850671c8328fd5d195363aaf61d49cd35dc28031
Aleksandar Cvejic <cvejicaca@gmail.com>
2019-06-09 09:31:14 +0200

    Initial project structure

---
+++ agentserver/src/main/java/com/megatravel/AgentserverApplication.java
@@ -0,0 +1,13 @@
+package com.megatravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentserverApplication.class, args);
+	}
+
+}
